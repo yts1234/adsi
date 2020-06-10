f682d97a2f08844a29ed161a9107b6af84085ae7
yts1234 <titoyosua@gmail.com>
2020-06-10 13:22:42 +0700

    Changelogs: 1. Start Implementing OOP on Disbursement.php 2. Add m_disbursement.php for DML (Data Manipulation Language)

--- disbursement.php
+++ disbursement.php
@@ -1,22 +1,21 @@
 <?php
 class disbursement 
+//class skeleton
 {
- //class skeleton
- public function callAPI(){
+ private $bank_code;
+ private $account_number;
+ private $amount;
+ private $remark;
 
+ public function __construct($bank_code, $account_number, 
+                             $amount, $remark){
+     $this->bank_code = $bank_code;
+     $this->account_number = $account_number;
+     $this->amount = $amount;
+     $this->remark = $remark;
  }
- 
- public function getStatus(){
 
- }
-
- public function postDisbursement(){
-
- }
-}
-
-//function to process the API call
-function callAPI($method, $url, $data){
+ public function callAPI($method, $url, $data){
     $curl = curl_init();
     switch ($method){
         case "POST":
@@ -45,27 +44,34 @@ function callAPI($method, $url, $data){
     if(!$result){die("Connection Failure");}
     curl_close($curl);
     return $result;
+ }
+ 
+ public function getStatus(){
+    $get_data=$this->callAPI('GET', 'https://nextar.flip.id/disburse/5535152564', false);
+    $response = json_decode($get_data, true);
+    print_r($response);//print the API response
+ }
+
+ public function postDisbursement(){
+    $data_array = array(
+        "bank_code"=>$this->bank_code,
+        "account_number"=>$this->account_number,
+        "amount"=>$this->amount,
+        "remark"=>$this->remark
+    );
+    
+    $url_data=http_build_query($data_array);//Urlify the array
+    $make_call = $this->callAPI('POST', 'https://nextar.flip.id/disburse', $url_data);
+    $response = json_decode($make_call, true);
+    return $response;
+ }
 }
 
-//GET API
-/* 
-$get_data=callAPI('GET', 'https://nextar.flip.id/disburse/5535152564', false);
-$response = json_decode($get_data, true);
-print_r($response);//print the API response
-*/
-//POST API
 
-$data_array = array(
-    "bank_code"=>"bni",
-    "account_number"=>"123456",
-    "amount"=>"10000",
-    "remark"=>"Transfer to Josh"
-);
-
-$url_data=http_build_query($data_array);//Urlify the array
-$make_call = callAPI('POST', 'https://nextar.flip.id/disburse', $url_data);
-$response = json_decode($make_call, true);
-print_r($response);
-
+$disburse = new disbursement("bni",1234,10000,"Test Disburse");
+print_r($disburse->postDisbursement());
+//require 'm_disbursement.php';
+//$trans2 = new M_disbursement($response);
+//print_r($trans->getData());
 
 ?>
---
+++ m_disbursement.php
@@ -0,0 +1,15 @@
+<?php
+
+class M_disbursement{
+    private $data;
+
+    function __construct($initData){
+        $this->data = $initData;
+    }
+
+    function getData(){
+        return $this->data[];
+    }
+}
+
+?>
