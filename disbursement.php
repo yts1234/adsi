4c12d9d17c0d5f54144f9d1573295263430adcb7
yts1234 <titoyosua@gmail.com>
2020-06-10 14:07:04 +0700

    Changelogs: 1. Created conditional statement, so we can execute the disbursement.php from terminal with argument. 2. Need to document it later.

--- disbursement.php
+++ disbursement.php
@@ -2,6 +2,7 @@
 class disbursement 
 //class skeleton
 {
+ /*
  private $bank_code;
  private $account_number;
  private $amount;
@@ -14,7 +15,8 @@ class disbursement
      $this->amount = $amount;
      $this->remark = $remark;
  }
-
+*/
+//Function to Call Slightly-big Flip API
  public function callAPI($method, $url, $data){
     $curl = curl_init();
     switch ($method){
@@ -45,19 +47,20 @@ class disbursement
     curl_close($curl);
     return $result;
  }
- 
- public function getStatus(){
-    $get_data=$this->callAPI('GET', 'https://nextar.flip.id/disburse/5535152564', false);
+ //Function to get the transaction status
+ public function getStatus($idTransaction){
+    $get_data=$this->callAPI('GET', 'https://nextar.flip.id/disburse/'.$idTransaction.'', false);
     $response = json_decode($get_data, true);
-    print_r($response);//print the API response
+    return $response;
  }
-
- public function postDisbursement(){
+//function to make a transaction
+ public function postDisbursement($bank_code, $account_number, 
+                                $amount, $remark){
     $data_array = array(
-        "bank_code"=>$this->bank_code,
-        "account_number"=>$this->account_number,
-        "amount"=>$this->amount,
-        "remark"=>$this->remark
+        "bank_code"=>$bank_code,
+        "account_number"=>$account_number,
+        "amount"=>$amount,
+        "remark"=>$remark
     );
     
     $url_data=http_build_query($data_array);//Urlify the array
@@ -67,9 +70,21 @@ class disbursement
  }
 }
 
+//this code is for checking the argument passed from the terminal
+if (isset($argc)){
+    $disburse = new disbursement();
+    if ($argv[1]=="disbursement") {
+        print_r($disburse->postDisbursement((string)$argv[2],(int)$argv[3],(int)$argv[4],(string)$argv[5]));        
+    }elseif($argv[1]=="status"){
+        print_r($disburse->getStatus((string)$argv[2]));
+    }else {
+        echo("Please check your argument");
+    }
+}
 
-$disburse = new disbursement("bni",1234,10000,"Test Disburse");
-print_r($disburse->postDisbursement());
+
+//print_r($disburse->postDisbursement());
+//print_r($disburse->getStatus(7315347024))
 //require 'm_disbursement.php';
 //$trans2 = new M_disbursement($response);
 //print_r($trans->getData());
